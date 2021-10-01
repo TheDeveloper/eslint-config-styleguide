5b71ce5a3efb6b2b1c3b406a5966a2cbe0206de4
Geoff Wagstaff <geoff@gosquared.com>
2021-10-01 20:48:32 +0000

    extend airbnb-base

---
+++ rules.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    node: true
+  },
+
+  parserOptions: {
+    ecmaVersion: 12
+  },
+
+  extends: [
+    "airbnb-base"
+  ],
+
+  rules: {
+    'comma-dangle': [ 'error', 'never' ]
+  }
+};
